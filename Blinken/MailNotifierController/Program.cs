dfad527a7007b3fdd0f381fc1132a8e1347c26f9
Ryan Janzen <rpwjanzen@gmail.com>
2012-07-24 16:39:05 -0600

    Added ability to set rgb values via command line.

--- Blinken/MailNotifierController/Program.cs
+++ Blinken/MailNotifierController/Program.cs
@@ -8,7 +8,20 @@ namespace MailNotifierController
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length == 3)
+            {
+                byte red = byte.Parse(args[0]);
+                byte green = byte.Parse(args[1]);
+                byte blue = byte.Parse(args[2]);
+
+                Color color = Color.FromArgb(red, green, blue);
+                var mailNotifier = new MailNotifier();
+                mailNotifier.SetColor(color);
+
+                Console.WriteLine("Color set to " + color.Name);
+
+            }
+            else if (args.Length != 1)
             {
                 return;
             }
@@ -35,11 +48,30 @@ namespace MailNotifierController
             else
             {
                 Color color = Color.FromName(args[0]);
-                var mailNotifier = new MailNotifier();
-                mailNotifier.SetColor(color);
+                byte red = RoundToNotifierColorValue(color.R);
+                byte green = RoundToNotifierColorValue(color.G);
+                byte blue = RoundToNotifierColorValue(color.B);
 
-                Console.WriteLine("Color set to " + color.Name);
+                var notifierColor = Color.FromArgb(red, green, blue);
+
+                var mailNotifier = new MailNotifier();
+                mailNotifier.SetColor(notifierColor);
+
+                Console.WriteLine("Color set to " + notifierColor.Name);
             }
+        }
+
+        private static byte RoundToNotifierColorValue(byte b)
+        {
+            byte newValue;
+            if (b == 0)
+                newValue = 0;
+            else if (b < 128)
+                newValue = 1;
+            else
+                newValue = 33;
+
+            return newValue;
         }
     }
 }
